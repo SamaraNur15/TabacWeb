ff232c1cd5bb0282f0104ffdba2d843253ea719e
Fernando <fer24sanchez@gmail.com>
2025-10-15 17:25:03 -0300

    chore: commit all changes (including deletions)

--- models/Usuario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const usuarioSchema = new mongoose.Schema({
-    nombre: String,
-    email: { type: String, unique: true },
-    password: String
-});
-
-module.exports = mongoose.model('Usuario', usuarioSchema);
--- public/user-id.js
+++
@@ -1,8 +0,0 @@
-window.getUserId = function () {
-  const saved = localStorage.getItem('userId');
-  if (saved) return saved;
-  // Para probar sin sorpresas, fijamos uno estable:
-  const nuevo = 'anon-1';
-  localStorage.setItem('userId', nuevo);
-  return nuevo;
-};
